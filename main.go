2163a22fbe2195af880b8f0d331904c491674930
Jason Barnett <jason.w.barnett@gmail.com>
2017-04-06 07:52:45 -0400

    more updates

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/base64"
+	"encoding/binary"
 	"encoding/pem"
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"math/big"
-	"os"
 	"strconv"
 
 	"github.com/davecgh/go-spew/spew"
@@ -27,7 +28,7 @@ import (
 //    <D>…</D>
 // </RSAKeyValue>
 
-type rsaKeyValue struct {
+type rsaParameters struct {
 	XMLName  xml.Name `xml:"RSAKeyValue"`
 	Modulus  string   `xml:"Modulus,omitempty"`
 	Exponent string   `xml:"Exponent,omitempty"`
@@ -53,11 +54,37 @@ func main() {
 
 	fmt.Printf("priv_key:\n---------\n\n%+v\n\n", privKey)
 	fmt.Print(spew.Sdump(privKey))
-	saveXML(privKey)
+	rsaToXML(privKey)
 }
 
-func saveXML(pk *rsa.PrivateKey) {
+func xmlToRSA(xml *rsaParameters) (pk *rsa.PrivateKey) {
+	// (*rsa.PrivateKey)(0xc4200723c0)({
+	//  PublicKey: (rsa.PublicKey) {
+	//   N: (*big.Int)(0xc42000c760)(302267542762161784760758685397913542247),
+	//   E: (int) 65537
+	//  },
+	//  D: (*big.Int)(0xc42000c780)(256782358717722162513073789955570047873),
+	//  Primes: ([]*big.Int) (len=2 cap=2) {
+	//   (*big.Int)(0xc42000c7a0)(17439525864715128359),
+	//   (*big.Int)(0xc42000c7c0)(17332325724160349633)
+	//  },
+	//  Precomputed: (rsa.PrecomputedValues) {
+	//   Dp: (*big.Int)(0xc42000c860)(17172625572631125829),
+	//   Dq: (*big.Int)(0xc42000c880)(9415263562656707009),
+	//   Qinv: (*big.Int)(0xc42000c8a0)(8750017907401368731),
+	//   CRTValues: ([]rsa.CRTValue) {
+	//   }
+	//  }
+	// })
+
+	pk.D = xml.Exponent
+
+	return pk
+}
+
+func rsaToXML(pk *rsa.PrivateKey) {
 	var n, e, d, p, q, dp, dq, qinv string
+
 	n = toBase64(pk.PublicKey.N)
 	e = toBase64(pk.PublicKey.E)
 	d = toBase64(pk.D)
@@ -71,11 +98,10 @@ func saveXML(pk *rsa.PrivateKey) {
 			fmt.Println("ERROR: more than 2 primes")
 		}
 	}
+
 	dp = toBase64(pk.Precomputed.Dp)
 	dq = toBase64(pk.Precomputed.Dq)
 	qinv = toBase64(pk.Precomputed.Qinv)
-
-	pk.Precomputed.Dp.Bytes()
 
 	fmt.Printf("n: %s\n", n)
 	fmt.Printf("e: %s\n", e)
@@ -86,7 +112,7 @@ func saveXML(pk *rsa.PrivateKey) {
 	fmt.Printf("dq: %s\n", dq)
 	fmt.Printf("qinv: %s\n", qinv)
 
-	rsaKey := &rsaKeyValue{
+	rsaKey := &rsaParameters{
 		Modulus:  n,
 		Exponent: e,
 		P:        p,
@@ -96,11 +122,11 @@ func saveXML(pk *rsa.PrivateKey) {
 		InverseQ: qinv,
 	}
 
-	enc := xml.NewEncoder(os.Stdout)
-	enc.Indent("", "  ")
-	if err := enc.Encode(rsaKey); err != nil {
+	xmlOutput, err := xml.MarshalIndent(rsaKey, "", "  ")
+	if err != nil {
 		fmt.Printf("error: %v\n", err)
 	}
+	fmt.Println(string(xmlOutput))
 
 	fmt.Print(spew.Sdump(rsaKey))
 }
@@ -132,4 +158,20 @@ func toBase64(i interface{}) (b64 string) {
 	}
 
 	return b64
+}
+
+func base64ToBigInt(b64 string) (i *big.Int) {
+	stdDec, _ := base64.StdEncoding.DecodeString(b64)
+	i.SetBytes(stdDec)
+	return i
+}
+
+func base64ToInt(b64 string) (i int) {
+	stdDec, _ := base64.StdEncoding.DecodeString(b64)
+	buf := bytes.NewReader(stdDec)
+	err := binary.Read(buf, binary.LittleEndian, &i)
+	if err != nil {
+		fmt.Println("binary.Read failed:", err)
+	}
+	return i
 }
