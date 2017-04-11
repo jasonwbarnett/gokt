73677823286c1abc64d4ab5e1f6b37292a256543
Jason Barnett <jason.w.barnett@gmail.com>
2017-04-11 10:13:51 -0400

    update release

--- release.sh
+++ release.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+
+tag=$1
+[[ -z $tag ]] && { echo "You must provide a tag, e.g. v0.1.0" 1>&2; exit 1; }
+
 github-release release \
     --user jasonwbarnett \
     --repo gokt \
-    --tag v0.1.0
+    --tag ${tag}
+
+gox
 
 for bin in gokt*; do
 	github-release upload \
 	    --user jasonwbarnett \
 	    --repo gokt \
-	    --tag v0.1.0 \
+	    --tag ${tag}
 	    --file $bin \
 	    --name $bin
 done
